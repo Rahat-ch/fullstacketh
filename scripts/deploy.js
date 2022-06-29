de8b14153cef8f63cafa6257991d7f15fecc1a0c
Rahat <rahat@rahatcodes.com>
2022-06-29 00:25:33 -0400

    add erc20 token

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,8 +14,12 @@ async function main() {
   const Token = await hre.ethers.getContractFactory("Token");
   const token = await Token.deploy();
 
+  const RCToken = await hre.ethers.getContractFactory("RCToken");
+  const rcToken = await RCToken.deploy("Nader Dabit Token", "NDT");
+
   await greeter.deployed();
   await token.deployed();
+  await rcToken.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
   console.log("Token deployed to:", token.address);
